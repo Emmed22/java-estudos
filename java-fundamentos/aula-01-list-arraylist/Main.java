e64771ebd98c920536fe472f489c8cbd923a83ae
Mariana Duarte <mybooksonly2021@gmail.com>
2026-08-10 14:25:53 -0300

    feat: adiciona exercício de foreach com lista de nomes

--- java-fundamentos/aula-01-list-arraylist/Main.java
+++ java-fundamentos/aula-01-list-arraylist/Main.java
@@ -2,15 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
-      
-        List<String> nomes = new ArrayList<>();
+    public static void main(String[] args) {
+        List <String> nomes = new ArrayList<>();
         nomes.add("Maria");
         nomes.add("João");
         nomes.add("Ana");
+        nomes.add("Carlos");
+        nomes.add("Fernanda");
+        nomes.add("Pedro");
+        nomes.add("Amanda");
 
-        for (String nome : nomes){
-            System.out.println("Olá " +nome);
+        for(String nome : nomes){
+            System.out.println("Olá " + nome +" !");
+
         }
     }
 }
