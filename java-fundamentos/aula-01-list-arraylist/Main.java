e40add0ca35b748925520dd7b3bf3bb9f97f3e13
Mariana Duarte <mybooksonly2021@gmail.com>
2026-08-11 22:00:09 -0300

    feat: adiciona foreach e contador

--- java-fundamentos/aula-01-list-arraylist/Main.java
+++ java-fundamentos/aula-01-list-arraylist/Main.java
@@ -10,9 +10,14 @@ public class Main {
         frutas.add("Uva");
         frutas.add("Morango");
 
-        for(int i =0; i<frutas.size(); i++){
-            System.out.println(i + " - "+ frutas.get(i)
-            );
-        }
-    }       
+        //for(int i =0; i<frutas.size(); i++){
+        //    System.out.println(i + " - "+ frutas.get(i)
+         //   );
+       // }
+       int contador = 0;
+       for ( String fruta : frutas){
+        System.out.println( contador+ " - " + fruta);
+        contador++;
+       }
+    }     
 }
